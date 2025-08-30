65d1559060cbeb16a99efb8d4cb2f209bffd69f4
catskill39 <catskill39@gmail.com>
2025-08-30 19:34:46 +0300

    Update murdermystery2.lua

--- murdermystery2.lua
+++ murdermystery2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/catskill39/Rbxz/refs/heads/main/Lite-Cheats-HUB/Loader/Loader.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/catskill39/cheat/refs/heads/main/HUB/MM2.lua"))()
